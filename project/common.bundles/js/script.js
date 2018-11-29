bec56eef5be999a178a5c6b5d9375ca0d34e8e36
boyarov0502 <poluboiarov@gmail.com>
2018-11-29 19:38:10 +0500

    Form test

---
+++ project/common.bundles/js/script.js
@@ -0,0 +1,42 @@
+$(function(){
+	'use strict';
+	$('.form').on('submit', function(e){
+		e.preventDefault();
+		var fd = new FormData( this );
+		$.ajax({
+			url: 'phpmailer/mail.php',
+			type: 'POST',
+			contentType: false,
+			processData: false,
+			data: fd,
+			success: function(msg){
+				console.log(fd)
+			}
+		});
+	});
+});
+
+$('.slider').slick({
+  dots: true
+});
+// Использует jQuery, должен быть загружен после document.ready
+
+$('.switcher').each(function switcherCheck(index) {
+    if ($('.switcher').eq(index).find('input').is(":checked")) {
+        $(this).addClass('checked');
+    }
+    else {
+        $(this).removeClass('checked');
+    }
+})
+$('.switcher input').click(function () {
+    $(this).parent('.switcher').toggleClass('checked');
+});
+var itemsLength = $('.list').children().length;
+
+if ( itemsLength > 10 && itemsLength < 21 ) {
+	$('.list').addClass('list_columns_two');
+}
+else if ( itemsLength >= 21 ) {
+	$('.list').addClass('list_columns_three');
+}
